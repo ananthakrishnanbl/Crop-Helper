4286321f0d10cb0458afa4a2fa7209888b1517ec
ananthakrishnanbl <ananthakrishnanbl07@gmail.com>
2025-11-22 16:01:37 +0530

    Mongodb sucessfully added and avatar change feature completed

--- constfiles/avatar/avatar.js
+++ constfiles/avatar/avatar.js
@@ -2,13 +2,15 @@ document.querySelector(".name").innerText=userData.username;
 document.title=userData.username;
 
 document.querySelector(".avatar").style.backgroundImage=`url("${avatar[userData.avatar_name]}")`;
+document.querySelector(".avatar-image").style.backgroundImage=`url("${avatar[userData.avatar_name]}")`;
+document.querySelector(".avatar-name").value=userData.avatar_name;
 
 const avatar_box=document.querySelector(".avatar-box");
 Object.keys(avatar).forEach((val,i)=>{
-    console.log(val);
     const avatar_image=document.createElement("div");
     avatar_image.classList.add("avatars");
     avatar_image.classList.add(`${val}`);
+    avatar_image.setAttribute("id",val);
     avatar_image.style.backgroundImage=`url("${avatar[val]}")`;
     avatar_box.append(avatar_image);
 })
@@ -27,4 +29,38 @@ avatar_images.forEach((val)=>{
             val.classList.remove("easeout");
         })
     })
+    val.addEventListener("click",(e)=>{
+        const avatar_name = e.target.getAttribute("id");
+        document.querySelector(".avatar-image").style.backgroundImage=`url("${avatar[avatar_name]}")`;
+        document.querySelector(".avatar-name").value=avatar_name;
+        document.querySelector(".form").style.display="inline-block";
+    })
+})
+
+const setting=document.querySelector(".setting3");
+const arrow=document.querySelector(".angle")
+setting.addEventListener("click",(e)=>{
+    if (setting.getAttribute("id")==="hidden"){
+        document.querySelector(".hidden-settings").style.visibility="visible";
+        document.querySelector(".hidden-settings").classList.add("appear");
+        setting.setAttribute("id","visible");
+        arrow.classList.add("angle-rotate");
+    }
+    else{
+        document.querySelector(".hidden-settings").classList.remove("appear");
+        setTimeout(()=>{
+            document.querySelector(".hidden-settings").style.visibility="hidden";
+        },1000);
+        setting.setAttribute("id","hidden");
+        arrow.classList.remove("angle-rotate");
+    }
+})
+
+document.querySelector(".close").addEventListener("click",()=>{
+    document.querySelector(".avatar-name").value=userData.avatar_name;
+    document.querySelector(".form").style.display="none";
+})
+document.querySelector(".no").addEventListener("click",()=>{
+    document.querySelector(".avatar-name").value=userData.avatar_name;
+    document.querySelector(".form").style.display="none";
 })
--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 import express from "express";
 import path from "path";
 import fs from "fs";
+import mongoose from "mongoose";
 import session from "express-session";
 import { fileURLToPath } from "url";
-import { userData , allUserPlots, avatar } from "./data.mjs";
+import { avatar } from "./data.mjs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app=express();
 const port=3000;
-let errorTry=0;
+
+
+// const dataStore = async()=>{
+//     await mongoose.connect("mongodb://127.0.0.1:27017/userData");
+//     const userSchema = mongoose.Schema({
+//         username:{
+//             type: String,
+//             required: true,
+//             unique: true
+//         },
+//         password:{
+//             type: String,
+//             required: true
+//         },
+//         avatar_name: String
+//     })
+//     const user = mongoose.model("user",userSchema);
+//     const newUser = new user({
+//         username: "nandana",
+//         password: "0803",
+//         avatar_name: "default"
+//     })
+//     await newUser.save();
+// }
+// dataStore();
+
+let userSchema;
+let user;
+let plotSchema;
+let plots;
 
 app.use(express.urlencoded({extended:true}));
 app.use(
   session({
-    secret: "123Why123",     // change this
+    secret: "123Why123",
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -35,7 +65,7 @@ app.get("/home/home.js",(req,res)=>{
 app.get("/login",(req,res)=>{
     const data=fs.readFileSync(path.join(__dirname,"constfiles/login/login.html"));
     if (!req.session.user){
-        if (errorTry){
+        if (req.session.errorTry){
             res.send(data.toString().replace("false","true"));
         }
         else{
@@ -53,21 +83,93 @@ app.get("/login/login.js",(req,res)=>{
     res.sendFile(path.join(__dirname,"constfiles/login/login.js"));
 })
 
-app.post("/login",(req,res)=>{
+
+/*-----------------Below Part of Progream that needs Mongodb -----------------*/ 
+//Setting up Mongodb 
+mongoose.connect("mongodb://127.0.0.1:27017/userData")
+.then(async ()=>{
+    userSchema = new mongoose.Schema({
+        username:{
+            type: String,
+            required: true,
+            unique: true
+        },
+        password:{
+            type: String,
+            required: true
+        },
+        avatar_name: String
+    })
+    user = mongoose.model("user",userSchema);
+    plotSchema = new mongoose.Schema({
+        userName: String,
+        plotCount: Number,
+        plots: [
+            {
+                plotID:String,
+                plotName: String,
+                country: String,
+                state: String,
+                district: String,
+                crop: String
+            }
+        ]
+    })
+    plots = mongoose.model("plots",plotSchema);
+})
+//Setting up Pages
+.then(()=>{
+
+app.post("/login",async (req,res)=>{
+    const userData =await user.find();
     const  { username , password } = req.body;
     req.session.user= userData.filter((val)=>{
         return val.username===username && val.password==password;
     })[0]
     console.log("username : "+username + "| password: " + password);
     if (req.session.user){
-        errorTry=0;
+        req.session.errorTry=0;
         res.redirect(`/${username}`);
     }
     else{
-        errorTry++;
+        req.session.errorTry=1;
+        res.redirect("/login");
+    }
+
+})
+
+app.get("/:username",async (req,res)=>{
+    if (req.session.user && req.session.user.username===req.params.username){
+        const userData =await user.find();
+        const username=req.session.user.username;
+        req.session.user= userData.filter((val)=>{
+            return val.username===username;
+        })[0]
+        let data=fs.readFileSync(path.join(__dirname,"constfiles/dashboard/dashboard.html"));
+        const allUserPlots = await plots.find()
+        req.session.userPlot = allUserPlots.filter((val)=>{
+            return val.userName===req.session.user.username;
+        })[0];
+        data = data.toString().replace("DATA",JSON.stringify(req.session.user));
+        data=data.replace("10101",req.session.userPlot.plotCount);
+        data=data.replaceAll("301",req.session.user.username);
+        data=data.replace("USERPLOTS",JSON.stringify(req.session.userPlot.plots));
+        data=data.replace("AVATARDATA",JSON.stringify(avatar));
+        res.send(data);
+    }
+    else{
         res.redirect("/login");
     }
 })
+
+app.post("/:username/avatar-change",async (req,res)=>{
+    console.log(req.body.avatar);
+    await user.updateOne({username:req.session.user.username},{$set:{avatar_name:req.body.avatar}});
+    res.redirect(`/${req.session.user.userName}`);
+})
+})
+/*-----------------Above Part of Progream that needs Mongodb -----------------*/ 
+
 
 app.get("/create-account",(req,res)=>{
     res.send("This Page is under construction")
@@ -94,23 +196,6 @@ app.get("/dashboard.css",(req,res)=>{
 })
 app.get("/dashboard.js",(req,res)=>{
     res.sendFile(path.join(__dirname,"constfiles/dashboard/dashboard.js"));
-})
-app.get("/:username",(req,res)=>{
-    if (req.session.user && req.session.user.username===req.params.username){
-        let data=fs.readFileSync(path.join(__dirname,"constfiles/dashboard/dashboard.html"));
-        req.session.userPlot = allUserPlots.filter((val)=>{
-            return val.userName===req.session.user.username;
-        })[0];
-        data = data.toString().replace("DATA",JSON.stringify(req.session.user));
-        data=data.replace("10101",req.session.userPlot.plotCount);
-        data=data.replaceAll("301",req.session.user.username);
-        data=data.replace("USERPLOTS",JSON.stringify(req.session.userPlot.plots));
-        data=data.replace("AVATARDATA",JSON.stringify(avatar));
-        res.send(data);
-    }
-    else{
-        res.redirect("/login");
-    }
 })
 app.get("/:username/chatbot",(req,res)=>{
     if (req.session.user && req.session.user.username===req.params.username){
